366ee04836962ecaa6fd8c1a7d4774f2ab223667
Leo Chen <a0952250818@gmail.com>
2018-11-12 02:56:19 +0800

    建立付款相關之元件

---
+++ src/app/checkout/payment-info/payment-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-info',
+  templateUrl: './payment-info.component.html',
+  styleUrls: ['./payment-info.component.css']
+})
+export class PaymentInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
