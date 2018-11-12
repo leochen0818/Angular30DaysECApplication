eae871b3d25abdd9a8d6c6dec920be634c124a32
Leo Chen <a0952250818@gmail.com>
2018-11-12 22:23:05 +0800

    新增路由定義檔

---
+++ src/app/app-path.const.ts
@@ -0,0 +1,35 @@
+export const appPath = Object.freeze({
+
+  // 首頁
+  home: '',
+
+  // 甜點
+  products: 'products',
+
+  // 登入
+  login: 'login',
+
+  // 購物車
+  cart: 'cart',
+
+  // 結帳
+  checkout: 'checkout',
+
+  // 結帳流程
+  checkoutFlow: {
+
+    // 運送
+    customerInfo: 'customer-info',
+
+    // 付款
+    paymentInfo: 'payment-info',
+
+    // 發票
+    receiptInfo: 'receipt-info'
+
+  },
+
+  // 結帳成功
+  success: 'suceess'
+
+});
