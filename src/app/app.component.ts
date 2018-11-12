3d49624dbab77a0d71257c61f3ae7fd0d3609b34
Leo Chen <a0952250818@gmail.com>
2018-11-12 23:46:09 +0800

    完成測試路由的 Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+// Constant
+import { appPath } from './app-path.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular30DaysECApplication';
+
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @memberof AppComponent
+   */
+  path = appPath;
+
 }
