87541ade560b834630add19ad648b9e994f6c067
Leo Chen <a0952250818@gmail.com>
2018-11-12 21:14:39 +0800

    把沒有關聯到的 Component 關聯起來

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
+import { PaymentInfoComponent } from './payment-info/payment-info.component';
+import { ReceiptInfoComponent } from './receipt-info/receipt-info.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CheckoutRoutingModule
   ],
-  declarations: [CheckoutComponent]
+  declarations: [
+    CheckoutComponent,
+    CustomerInfoComponent,
+    PaymentInfoComponent,
+    ReceiptInfoComponent
+  ]
 })
 export class CheckoutModule { }
--- src/app/product-section/product-section.module.ts
+++ src/app/product-section/product-section.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductSectionRoutingModule } from './product-section-routing.module';
 import { ProductSectionComponent } from './product-section.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductSectionRoutingModule
   ],
-  declarations: [ProductSectionComponent]
+  declarations: [ProductSectionComponent, ProductListComponent]
 })
 export class ProductSectionModule { }
