0f7919a78b2261c6c5b8e946c1f6c4f06d9c0652
Leo Chen <a0952250818@gmail.com>
2018-11-14 23:48:42 +0800

    完成運送區塊的套版

--- src/app/checkout/customer-info/customer-info.component.ts
+++ src/app/checkout/customer-info/customer-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { appPath } from 'src/app/app-path.const';
 
 @Component({
   selector: 'app-customer-info',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-info.component.css']
 })
 export class CustomerInfoComponent implements OnInit {
+
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @memberof CustomerInfoComponent
+   */
+  path = appPath;
 
   constructor() { }
 
