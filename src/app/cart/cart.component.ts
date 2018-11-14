d07e2c6c0f2e92daa06f023510c8f77ced9958d4
Leo Chen <a0952250818@gmail.com>
2018-11-14 22:45:55 +0800

    完成購物車頁面的套版

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { appPath } from '../app-path.const';
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @memberof CartComponent
+   */
+  path = appPath;
 
   constructor() { }
 
