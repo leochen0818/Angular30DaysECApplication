77e3852b2cb196e1bf48c61b26fa941455722992
Leo Chen <a0952250818@gmail.com>
2018-11-16 00:05:21 +0800

    完成發票區塊的套版

--- src/app/checkout/receipt-info/receipt-info.component.ts
+++ src/app/checkout/receipt-info/receipt-info.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Constant
+import { appPath } from 'src/app/app-path.const';
+
+// Enum
+import { SendType } from './send-type.enum';
+
 @Component({
   selector: 'app-receipt-info',
   templateUrl: './receipt-info.component.html',
@@ -7,9 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptInfoComponent implements OnInit {
 
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @memberof ReceiptInfoComponent
+   */
+  path = appPath;
+
+  /**
+   * 給 Template 用的寄送方式列舉
+   *
+   * @memberof ReceiptInfoComponent
+   */
+  sendType = SendType;
+
+  /**
+   * 當前的寄送類型，預設使用 Email
+   *
+   * @memberof ReceiptInfoComponent
+   */
+  selectedType = SendType.EMAIL;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * 切換寄送類型
+   *
+   * @param {number} type - 欲切換的寄送類型
+   * @memberof ReceiptInfoComponent
+   */
+  switch(type: number): void {
+    this.selectedType = type;
+  }
+
+  /**
+   * 傳入的寄送類型是否為當前所選擇的寄送類型
+   *
+   * @param {number} type - 欲判斷的寄送類型
+   * @returns {boolean}
+   * @memberof ReceiptInfoComponent
+   */
+  didSelected(type: number): boolean {
+    return this.selectedType === type;
   }
 
 }
